c40cfd16960d7608371149a4d75106ecc79c579c
muhamad aldian rizki <m.aldian@paxel.co>
2019-08-13 10:44:39 +0700

    test backend

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,121 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click="login()">login</button>
+    <button @click="logout()">logout</button>
+    <button @click="post()">post</button>
+    <button @click="get()">get</button>
+    <button @click="put()">put</button>
+    <button @click="del()">delete</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String,
+    msg: String
   },
+  created() {},
+  methods: {
+    login() {
+      axios({
+        method: "post",
+        url: "http://localhost:8080/login",
+        data: {
+          username: "admin",
+          password: "admin"
+        }
+      }).then(function(response) {
+        console.log(response.data.data);
+      });
+    },
+    logout() {
+      axios({
+        method: "post",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.t-IDcSemACt8x4iTMCda8Yhe3iZaWbvV5XKSTbuAn0M"
+        },
+        url: "http://localhost:8080/logout"
+      }).then(function(response) {
+        console.log(response.data.data);
+      });
+    },
+    post() {
+      axios({
+        method: "post",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.t-IDcSemACt8x4iTMCda8Yhe3iZaWbvV5XKSTbuAn0M"
+        },
+        url: "http://localhost:8080/benefits",
+        data: {
+          title: {
+            id: "Mengantarkan Dengan Dampak",
+            eng: "Delivery The Impact"
+          },
+          description: {
+            id: "Lorem Ipsum Dolor Sit Amet",
+            eng: "Lorem Ipsum Dolor Sit Amet"
+          },
+          imagevideo: "Link",
+          status: "Inactive"
+        }
+      }).then(function(response) {
+        console.log(response.data.data);
+      });
+    },
+    put() {
+      axios({
+        method: "put",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.t-IDcSemACt8x4iTMCda8Yhe3iZaWbvV5XKSTbuAn0M"
+        },
+        url: "http://localhost:8080/benefits/5d4a46aa4a6493cc346b6c66",
+        data: {
+          title: {
+            id: "Mengantarkan Dengan Dampak",
+            eng: "Delivery The Impact"
+          },
+          description: {
+            id: "Lorem Ipsum Dolor Sit Amet",
+            eng: "Lorem Ipsum Dolor Sit Amet"
+          },
+          imagevideo: "Link",
+          status: "Inactive"
+        }
+      }).then(function(response) {
+        console.log(response.data.data);
+      });
+    },
+    get() {
+      axios({
+        method: "get",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.t-IDcSemACt8x4iTMCda8Yhe3iZaWbvV5XKSTbuAn0M"
+        },
+        url: "http://localhost:8080/benefits"
+      }).then(function(response) {
+        console.log(response.data.data);
+      });
+    },
+    del() {
+      axios({
+        method: "delete",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.t-IDcSemACt8x4iTMCda8Yhe3iZaWbvV5XKSTbuAn0M"
+        },
+        url: "http://localhost:8080/benefits/5d4a46aa4a6493cc346b6c66"
+      }).then(function(response) {
+        console.log(response.data.data);
+      });
+    }
+  }
 };
 </script>
 
